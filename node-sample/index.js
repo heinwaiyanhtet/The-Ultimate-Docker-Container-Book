8583d4b87871d5ebc47d3b72e3313dba68b680f7
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-02-21 10:57:38 +0630

    added existing code in container

--- node-sample/index.js
+++ node-sample/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const hobbies = 
+[
+    'Swimming', 'Diving', 'Jogging', 'Cooking', 'Singing'
+];
+
+app.get('/hobbies', (req,res) =>
+{
+    res.send(hobbies);
+})
+
+
 app.listen(3000,'0.0.0.0',() => {
     console.log("application is listening at 0.0.0.0:3000");
 })
@@ -8,3 +19,4 @@ app.listen(3000,'0.0.0.0',() => {
 app.get('/',(req,res) => {
     res.send('Sample application: Hello World!');
 })
+
