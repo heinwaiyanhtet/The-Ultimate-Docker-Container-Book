2f464168155f8c4b3181ace7ce3c2b0e1365a3c1
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-02-21 10:28:39 +0630

    node

--- node-sample/index.js
+++ node-sample/index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const app = express();
 
-const hobbies = [
-    'Swimming', 'Diving', 'Jogging', 'Cooking', 'Singing'
-];
-
-app.get('/hobbies', (req,res)=>{
-    res.send(hobbies);
-})
-
 app.listen(3000,'0.0.0.0',() => {
-    console.log("Application listening at 0.0.0.0:3000")
+    console.log("application is listening at 0.0.0.0:3000");
 })
 
 app.get('/',(req,res) => {
-    res.send('Sample Application: Hello World!');
+    res.send('Sample application: Hello World!');
 })
